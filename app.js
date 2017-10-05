d6ebfc268830c9d046efecfed3dbd5ba6570b241
johana <jovitta26@gmail.com>
2017-10-05 15:38:37 -0300

    ejercicio 1

---
+++ app.js
@@ -0,0 +1,14 @@
+function cifrado(){
+    do}
+      var resp = prompt(" indique el numero de lo que desea hacer: 1)cifrar - 2)descifrar");
+        if(resp !=""){
+         if(resp == "1") {
+          cifrar()
+          } else if (resp == "2")
+            descifrar();
+            } else {
+             alert ("Ingrese una opcion valida");
+         }
+
+        while (resp =="" || (resp !="1" && resp!="2"));
+        }
